61a755ba03f5263520d4c25bce1dc27b90939d3f
Vincent <electricplasma411@gmail.com>
2022-02-08 19:34:21 -0500

    file edited

---
+++ Lab1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int input_number;
+  std::cout << "Hello World!\n";
+  std::cin >> input_number;
+  std::cout << "Cool"<< input_number;
+  
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0;
+  return 0;
+}
